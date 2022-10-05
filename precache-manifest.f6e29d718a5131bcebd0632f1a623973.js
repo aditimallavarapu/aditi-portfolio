2b2852fad43283a8f6ecd3abdb794ebab3fef3ed
aditimallavarapu <m.aditi89@gmail.com>
2022-10-05 00:50:32 -0400

    Updates

--- precache-manifest.f6e29d718a5131bcebd0632f1a623973.js
+++ precache-manifest.f6e29d718a5131bcebd0632f1a623973.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4169e959e43db9a436cfe77f95ebe4ef",
+    "revision": "cbbe960602d7b95b9e7dd7a7600e57b6",
     "url": "/aditi-portfolio/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/aditi-portfolio/static/css/2.cc208727.chunk.css"
   },
   {
-    "revision": "55db0ff7bf74e640facb",
+    "revision": "dba080af9099f1e6c20c",
     "url": "/aditi-portfolio/static/css/main.0487006b.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/aditi-portfolio/static/js/2.aa3af4af.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "55db0ff7bf74e640facb",
-    "url": "/aditi-portfolio/static/js/main.eb96df38.chunk.js"
+    "revision": "dba080af9099f1e6c20c",
+    "url": "/aditi-portfolio/static/js/main.bd3c933a.chunk.js"
   },
   {
     "revision": "fa22cb0a881afe412138",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aditi-portfolio/precache-manifest.09b2aa2c5218d3609d664e4254ff399a.js"
+  "/aditi-portfolio/precache-manifest.f6e29d718a5131bcebd0632f1a623973.js"
 );
 
 self.addEventListener('message', (event) => {
