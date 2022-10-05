368cfeae2c432b6628dd643c193220a121c12435
aditimallavarapu <m.aditi89@gmail.com>
2022-10-05 00:41:03 -0400

    missing rows

--- src/components/projects.js
+++ src/components/projects.js
@@ -36,8 +36,10 @@ class Projects extends React.Component {
                 <div className="line-mf"></div>
               </div>
             </div>
+            <div className="row">
             <div className="title-box text-left">
                 <h3 className="title" style={{color:"white", marginTop: "10px", marginBottom:"10px"}}>Recent Projects</h3>
+          </div>
           </div>
           <div className="row">
             <div className="col-md-4">
@@ -138,8 +140,10 @@ class Projects extends React.Component {
             
             
           </div>
+          <div className="row">
           <div className="title-box text-left">
                 <h3 className="title" style={{color:"white", marginTop: "10px", marginBottom:"10px"}}>Doctoral Projects</h3>
+          </div>
           </div>
           <div className="row">
           <div className="col-md-4">
@@ -185,8 +189,10 @@ class Projects extends React.Component {
               </div>
             </div>
             </div>
+            <div className="row">
             <div className="title-box text-left">
                 <h3 className="title" style={{color:"white", marginTop: "10px", marginBottom:"10px"}}>Other Projects</h3>
+          </div>
           </div>
             <div className="row">
             <div className="col-md-4">
@@ -400,9 +406,10 @@ class Projects extends React.Component {
               </div>
             </div>
           </div>
-
+          <div className="row">
           <div className="title-box text-left">
                 <h3 className="title" style={{color:"white", marginTop: "10px", marginBottom:"10px"}}>Older Projects</h3>
+          </div>
           </div>
           <div className="row">
           <div className="col-md-4">
